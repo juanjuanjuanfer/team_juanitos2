4fc0ed1de657ba65aa0437e6e1610920676173ae
priscila <2109148@upy.edu.mx>
2022-04-05 17:10:52 -0500

    printarray.h

--- printarray.h
+++ printarray.h
@@ -2,7 +2,7 @@ void ImprimeMatriz(int arr[], int length)
 {
   int ImprimeMatriz=0;
  for (int i=0; i<length; ++i){
-        printf("%d",arr[i]); 
+        printf("%d ",arr[i]); 
      }
      
      return;
